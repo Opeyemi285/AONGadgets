26d81c078a1034023da4185e250c74dd2900b5c8
Opeyemi285 <abimbolaabdulkabir222@gmail.com>
2025-09-14 21:17:24 +0100

    commit

--- AONGadgets/products.js
+++ AONGadgets/products.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
       const menu = document.getElementById("menu");
       const closeMenu = document.getElementById("closeMenu");
       const mobileMenu = document.querySelector("div.mobile-menu");
+      const scrollLeft = document.querySelector(".left");
+      const scrollRight = document.querySelector(".right");
       // Show mobile menu
       menu.addEventListener("click", () => {
         mobileMenu.style.display = "flex";
@@ -9,4 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
       closeMenu.addEventListener("click", () => {
         mobileMenu.style.display = "none";
       });
-})
+
+      scrollRight.addEventListener("click", () => {
+      scrollContainer.scrollBy({
+      left: scrollItemMargin,
+      behavior: "smooth"
+      });
+      })
+      scrollLeft.addEventListener("click", () => {
+      scrollContainer.scrollBy({
+      left: -scrollItemMargin,
+      behavior: "smooth"
+      });
+});
+
+});
